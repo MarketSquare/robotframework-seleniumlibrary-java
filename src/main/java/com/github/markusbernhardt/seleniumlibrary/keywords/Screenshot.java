bcafb90b2ad16e0e9b08ac67ae6242d9b3f85226
Hi-Fi <juho.saarinen@gmail.com>
2017-11-26 17:34:21 +0200

    Added possibility to set directory for screenshots Documentation to libdoc

--- src/main/java/com/github/markusbernhardt/seleniumlibrary/keywords/Screenshot.java
+++ src/main/java/com/github/markusbernhardt/seleniumlibrary/keywords/Screenshot.java
@@ -31,36 +31,36 @@ public class Screenshot extends RunOnFailureKeywordsAdapter {
 	 */
 	@Autowired
 	protected Logging logging;
+	
+	@Autowired Robot robot;
+	
+	private static File screenshotDir = null;
 
 	// ##############################
 	// Keywords
 	// ##############################
-	@RobotKeywordOverload
-	public void capturePageScreenshot() {
-		capturePageScreenshot(null);
-	}
 
-	/**
-	 * Take a screenshot of the current page and embed it into the log.<br>
-	 * <br>
-	 * The <b>filename</b> argument specifies the name of the file to write the
-	 * screenshot into. If no filename is given, the screenshot is saved into
-	 * file selenium-screenshot-&lt;counter&gt;.png under the directory where
-	 * the Robot Framework log file is written into. The filename is also
-	 * considered relative to the same directory, if it is not given in absolute
-	 * format.<br>
-	 * <br>
-	 * A CSS can be used to modify how the screenshot is taken. By default the
-	 * background color is changed to avoid possible problems with background
-	 * leaking when the page layout is somehow broken.<br>
-	 * 
-	 * @param filename
-	 *            Default=NONE. Name of the file to write.
-	 */
-	@RobotKeyword
+	@RobotKeyword("Sets the directory for captured screenshots.\r\n" + 
+	        "\r\n" + 
+	        "``path`` argument specifies the absolute path to a directory where the screenshots should be written to. If the directory does not exist, it will be created. The directory can also be set when `importing` the library. If it is not configured anywhere, screenshots are saved to the same directory where Robot Framework's log file is written.\r\n" + 
+	        "\r\n" + 
+	        "The previous value is returned and can be used to restorethe original value later if needed.")
+	@ArgumentNames({"path"})
+	public String setScreenshotDirectory(String path) {
+	    String oldDir = screenshotDir != null ? screenshotDir.getAbsolutePath() : logging.getLogDir().getAbsolutePath();
+	    screenshotDir = new File(path);
+	    return oldDir;
+	}
+	
+	@RobotKeyword("Take a screenshot of the current page and embed it into the log.\r\n" + 
+	        "\r\n" + 
+	        "The ``filename`` argument specifies the name of the file to write the screenshot into. If no filename is given, the screenshot is saved into file selenium-screenshot-<counter>.png under the directory where the Robot Framework log file is written into. The filename is also considered relative to the same directory, if it is not given in absolute format.\r\n" + 
+	        "\r\n" + 
+	        "A CSS can be used to modify how the screenshot is taken. By default the background color is changed to avoid possible problems with background leaking when the page layout is somehow broken.")
 	@ArgumentNames({ "filename=NONE" })
-	public void capturePageScreenshot(String filename) {
-		File logdir = logging.getLogDir();
+	public void capturePageScreenshot(String...params) {
+	    String filename = robot.getParamsValue(params, 0, null);
+		File logdir = screenshotDir != null ? screenshotDir : logging.getLogDir();
 		File path = new File(logdir, normalizeFilename(filename));
 		String link = Robotframework.getLinkPath(path, logdir);
 		WebDriver currentWebDriver = browserManagement.getCurrentWebDriver();
@@ -69,17 +69,17 @@ public class Screenshot extends RunOnFailureKeywordsAdapter {
 		    logging.warn("HTMLunit is not supporting screenshots.");
 		    return;
 		} else {
-    		TakesScreenshot takesScreenshot = ((TakesScreenshot) currentWebDriver);
-    		if (takesScreenshot == null) {
-    			logging.warn("Can't take screenshot. No open browser found");
-    			return;
-    		}
-    
-    		byte[] png = takesScreenshot.getScreenshotAs(OutputType.BYTES);
-    		writeScreenshot(path, png);
-    
-    		logging.html(String.format(
-    				"</td></tr><tr><td colspan=\"3\"><a href=\"%s\"><img src=\"%s\" width=\"800px\"></a>", link, link));
+		    try {
+		        TakesScreenshot takesScreenshot = ((TakesScreenshot) currentWebDriver);
+		        byte[] png = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+	            writeScreenshot(path, png);
+	    
+	            logging.html(String.format(
+	                    "</td></tr><tr><td colspan=\"3\"><a href=\"%s\"><img src=\"%s\" width=\"800px\"></a>", link, link));
+		    } catch (NullPointerException e) {
+	            logging.warn("Can't take screenshot. No open browser found");
+                return;    
+	        }    	
 		}
 	}
 
